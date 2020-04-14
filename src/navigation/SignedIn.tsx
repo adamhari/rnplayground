c9434f2bd1a320d5d858aed78cd18265b65f8ef9
Adam <adam.hari@mobilityquotient.com>
2020-04-14 14:09:23 -0600

    refactor SignedIn

--- src/navigation/SignedIn.tsx
+++ src/navigation/SignedIn.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {StyleSheet} from "react-native";
-import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
+import {createStackNavigator, StackNavigationProp} from '@react-navigation/stack';
 import {createDrawerNavigator, DrawerContentScrollView, DrawerItem} from "@react-navigation/drawer";
 import {DrawerContentComponentProps} from "@react-navigation/drawer/lib/typescript/src/types";
 import Animated from "react-native-reanimated";
@@ -53,24 +53,25 @@ export default () => {
 		outputRange: [0, 16],
 	});
 
-	const animatedStyle = { borderRadius, transform: [{ scale }] };
+	const animatedStyle = {borderRadius, transform: [{scale}]};
 
 	return (
-		<LinearGradient style={{ flex: 1 }} colors={['#E94057', '#4A00E0']}>
+		<LinearGradient style={{flex: 1}} colors={['#E94057', '#4A00E0']}>
 			<Drawer.Navigator
 				// hideStatusBar
 				drawerType="slide"
 				overlayColor="transparent"
 				drawerStyle={styles.drawerStyles}
-				sceneContainerStyle={{ backgroundColor: 'transparent' }}
+				sceneContainerStyle={{backgroundColor: 'transparent'}}
 				drawerContent={(props: DrawerContentComponentProps) => {
 					setProgress(props.progress);
 					return <DrawerContent {...props} />;
-				}}>
+				}}
+			>
 				<Drawer.Screen name={'Screens'}>
 					{
 						props => (
-							<Animated.View style={StyleSheet.flatten([styles.stack, { borderRadius, transform: [{ scale }] }])}>
+							<Animated.View style={StyleSheet.flatten([styles.stack, animatedStyle])}>
 								<Stack.Navigator headerMode={'none'} screenOptions={{gestureEnabled: false}}>
 									<Stack.Screen name={'Home'} component={HomeScreen} />
 									<Stack.Screen name={'Another'} component={AnotherScreen} />
@@ -97,5 +98,9 @@ const styles = StyleSheet.create({
 		// shadowRadius: 10,
 		// elevation: 5,
 	},
-	drawerStyles: { flex: 1, width: '50%', backgroundColor: 'transparent' },
+	drawerStyles: {
+		flex: 1,
+		width: '50%',
+		backgroundColor: 'transparent'
+	},
 });
