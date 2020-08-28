ef8b2ed6a4014a9e2fe802710cdcfda5663cf256
Adam Hari <adamhari25@hotmail.com>
2020-08-28 08:58:02 -0600

    change compass to use react-native-compass-heading

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import {connect, ConnectedProps} from 'react-redux';
 import { RouteProp } from '@react-navigation/native';
 import {DrawerNavigationProp} from "@react-navigation/drawer";
+import CompassHeading from 'react-native-compass-heading';
 
 import {tapped} from '../../store/slices/user';
 import {ReduxState} from "../../store";
-import {TouchableOpacity} from "react-native-gesture-handler";
 import {LoggedInDrawerParamList} from "../../navigation/LoggedIn";
 
 const mapStateToProps = (state: ReduxState) => ({
@@ -30,19 +30,52 @@ type PropsFromNavigation = {
 
 type Props = PropsFromRedux & PropsFromNavigation;
 
-type State = {};
+type State = {
+	degree: number
+};
 
 class Home extends React.Component<Props, State> {
 
-	onPress = () => this.props.tapped();
+	constructor(props: Props) {
+		super(props);
+
+		this.state = {
+			degree: 30
+		};
+
+
+	}
+
+	async componentDidMount() {
+		await this.initializeCompass();
+	}
+
+	componentWillUnmount() {
+		CompassHeading.stop();
+	}
+
+	initializeCompass = async () => {
+		await CompassHeading.start(3, degree => {
+			this.setState({degree});
+		});
+	};
 
 	render() {
+		const {degree} = this.state;
+
 		return (
 			<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-				<TouchableOpacity onPress={this.onPress}>
-					<Text>tap</Text>
-				</TouchableOpacity>
-				<Text>taps: {this.props.taps.toString()}</Text>
+				<Image
+					style={[
+						{
+							width: '90%',
+							alignSelf: 'center'
+						},
+						{transform: [{rotate: `${360 - degree}deg`}]},
+					]}
+					resizeMode="contain"
+					source={require('../../assets/compass.png')}
+				/>
 			</View>
 		)
 	}
